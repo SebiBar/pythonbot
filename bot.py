7f55da21340ebe98b32b30ecca64efbbe764c873
Sebi <75215008+Sebipp@users.noreply.github.com>
2021-04-04 13:28:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,53 +5,53 @@ from discord.utils import get
 bot = commands.Bot(command_prefix=".")
 
 
-@bot.event      #STARTUP 
+@bot.event
 async def on_ready():
     await bot.change_presence(
         status=discord.Status.do_not_disturb, 
         activity=discord.Activity(type=discord.ActivityType.listening, name="@Mafiot"))
     print("neatza bro")
 
-@bot.event      #ERROR MESSAGE
+@bot.event
 async def on_command_error(ctx, error):
     await ctx.send(f"An error occured: {str(error)}")
 
-@bot.command()      #ALO MSG
+@bot.command()
 async def alo(ctx):
     await ctx.send("alo?")
 
-@bot.command()      #PURGE AMOUT MESSAGES
+@bot.command()
 @commands.has_permissions(administrator=True)
 async def clear(ctx, amout=2):
         await ctx.channel.purge(limit=amout+1)
 
-@bot.command()      #CHANGE NICK
+@bot.command()
 async def nick(ctx, member: discord.Member, *, nick):
     await member.edit(nick=nick)
     await ctx.send(f'Nickname was changed for {member.mention}.')
 
-@bot.command()      #SKRILLEX REMIX LINK
+@bot.command()
 async def skrillexremix(ctx):
     await ctx.send("https://www.youtube.com/watch?v=c5Myyr6vNxg")
 
-@bot.command()      #AVATAR
+@bot.command()
 async def avatar(ctx, *,  user: discord.Member =None):
     userAvatarUrl = user.avatar_url
     await ctx.send(userAvatarUrl)
 
-@bot.command()      #SEND MSG
+@bot.command()
 async def msg(ctx, member: discord.Member, *, content):
     await ctx.channel.purge(limit=1)
     dm = await member.create_dm()
     await dm.send(content)
     await ctx.send("Message sent!")
 
-@bot.command()      #REACT TO A MESSAGE
+@bot.command()
 async def react(ctx, message: discord.Message, emote):
     await message.add_reaction(emote)
     await ctx.send("Succesfully reacted.")
 
-@bot.command()      # WIDTH x HEIGHT + SIZE OF ATTACHMENT
+@bot.command()
 async def size(ctx):
     attachment = ctx.message.attachments[0] 
     await ctx.send(f"{attachment.width} x {attachment.height} ({attachment.size/1000000} MB)")
